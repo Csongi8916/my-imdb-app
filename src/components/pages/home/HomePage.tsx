172d063b4866b9f966fb0fedebe522dc787ae9c2
cskosolution <cskosolution@gmail.com>
2020-09-24 09:38:37 +0200

    Basic components are created and app is organized to copmonents

---
+++ src/components/pages/home/HomePage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './HomePage.module.css';
+
+export default function HomePage() {
+  return <div className='Root'></div>;
+}
---
+++ src/components/shared/movie-search-input/MovieSearchInput.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function MovieSearchInput() {
+  return <div className='Root'></div>;
+}
